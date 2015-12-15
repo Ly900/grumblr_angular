f2c3323c062d46006e00129ea019778433d20cf1
Ly Nguyen <Ly900@aol.com>
2015-12-15 15:49:52 -0500

    add grumbles into index controller; create form renders

--- js/app.js
+++ js/app.js
@@ -14,11 +14,13 @@
       $stateProvider
       .state("grumbleIndex", {
         url: "/grumbles",
-        templateUrl: "js/grumbles/index.html"
+        templateUrl: "js/grumbles/index.html",
+        controller: "GrumbleIndexController",
+        controllerAs: "GrumbleIndexViewModel"
       })
       .state("grumbleShow", {
-      url: "/grumbles/:id",
-      templateUrl: "js/grumbles/show.html"
-      });
+        url: "/grumbles/:id",
+        templateUrl: "js/grumbles/show.html"
+        });
     }
 })();
